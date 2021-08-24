65ce3fbc750cd3d0fb222c02d9d303d19587febe
anton <anton.larsen@persequor.com>
2021-08-24 09:00:00 +0200

    Removed default "/" controller path, to force people to consider what path a controller is supposed to use

--- core/src/main/java/io/varhttp/Controller.java
+++ core/src/main/java/io/varhttp/Controller.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Controller {
-	String path() default "/";
+	String path();
 	HttpMethod[] httpMethods() default {};
 }
