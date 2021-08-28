7b6c9e8743b59b77e9f7a71e1959742d8c0fe5a4
anton <anton.larsen@persequor.com>
2021-08-28 13:32:09 +0200

    added getMap method to request parameters

--- core/src/main/java/io/varhttp/RequestParameters.java
+++ core/src/main/java/io/varhttp/RequestParameters.java
@@ -1,7 +1,11 @@
 package io.varhttp;
 
+import java.util.List;
+import java.util.Map;
+
 public interface RequestParameters {
 	String get(String name);
 	void remove(String name);
 	boolean contains(String name);
+	Map<String, List<String>> getMap();
 }
--- core/src/main/java/io/varhttp/RequestParametersImplementation.java
+++ core/src/main/java/io/varhttp/RequestParametersImplementation.java
@@ -1,6 +1,12 @@
 package io.varhttp;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RequestParametersImplementation implements RequestParameters {
 	private HttpServletRequest request;
@@ -23,5 +29,10 @@ public class RequestParametersImplementation implements RequestParameters {
 	@Override
 	public boolean contains(String name) {
 		return request.getParameterMap().containsKey(name);
+	}
+
+	@Override
+	public Map<String, List<String>> getMap() {
+		return request.getParameterMap().entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> Arrays.asList(e.getValue())));
 	}
 }
