1d1b7c907a97991692c8cb139a673aaf829536ca
andre <andre.vigneaux@persequor.com>
2021-09-16 13:45:01 +0200

    when adding parameter handlers if it cannot add a new one due to already existing one in that priority, it should throw. otherwise it would not add it and say nothing and you wouldn't know why your handler is not there

--- core/src/main/java/io/varhttp/ParameterHandler.java
+++ core/src/main/java/io/varhttp/ParameterHandler.java
@@ -2,47 +2,19 @@ package io.varhttp;
 
 import io.varhttp.parameterhandlers.IParameterHandler;
 import io.varhttp.parameterhandlers.IParameterHandlerMatcher;
-
-import javax.enterprise.util.TypeLiteral;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.function.Function;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 @Singleton
 public class ParameterHandler {
-	private final List<PathVariableInfo> pathVariables = new ArrayList<>();
-	private Pattern pattern;
-	private Pattern pathVariablePattern = Pattern.compile("\\{(.*)\\}");
 	private final Serializer serializer;
 	private final ParameterHandlerMatcherFactory handlerMatcherFactory;
 	private final SortedSet<IParameterHandlerMatcher> parameterHandlers = new TreeSet<>();
@@ -54,7 +26,11 @@ public class ParameterHandler {
 	}
 
 	public void addParameterHandler(Class<? extends IParameterHandlerMatcher> handlerMatcher) {
-		parameterHandlers.add(handlerMatcherFactory.get(handlerMatcher));
+		if (!parameterHandlers.add(handlerMatcherFactory.get(handlerMatcher))) {
+			throw new IllegalArgumentException("Priority for: " + handlerMatcher.getName()
+					+ " already exists. The current parameter handler priorities are: \n"
+					+ parameterHandlers.stream().map(parameterHandler -> parameterHandler.getClass().getName() + " -> " + parameterHandler.getPriority()).collect(Collectors.joining("\n")));
+		}
 	}
 
 	public Set<HttpMethod> initializeHttpMethods(Method method) {
@@ -89,23 +65,4 @@ public class ParameterHandler {
 		}
 	}
 
-
-
-
-
-
-
-//	public Function<ControllerContext, Object>[] addPathVariables(Function<ControllerContext, Object>[] handlers, HttpServletRequest request) {
-//		Function<ControllerContext, Object>[] args = handlers.clone();
-//		String fullPath = request.getServletPath()+request.getPathInfo();
-//		Matcher m = pattern.matcher(fullPath);
-//		if (m.matches()) {
-//			for (int i = 0; i < pathVariables.size(); i++) {
-//				Class<?> type = pathVariables.get(i).getType();
-//				String value = m.group(i + 1);
-//				args[pathVariables.get(i).getArgno()] = (r -> convert(value, type, null));
-//			}
-//		}
-//		return args;
-//	}
 }
