cb6aafc842d62fe88ee73b87c1d692bfdf6907d7
roman <roman.sarakvasa@persequor.com>
2021-10-29 14:13:34 +0200

    Remove unnecessary helpers in Serializer simplifying the implementation on the other side

--- core/src/main/java/io/varhttp/Serializer.java
+++ core/src/main/java/io/varhttp/Serializer.java
@@ -6,13 +6,8 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public interface Serializer {
-	String serialize(Object content, String contentType);
 
 	void serialize(Writer writer, Object content, String contentType);
-
-	<T> T deserialize(String content, Class<T> clazz, String contentType);
-
-	<T> T deserialize(Reader content, Class<T> clazz, String contentType);
 
 	<T> T deserialize(Reader content, Type type, String contentType);
 
