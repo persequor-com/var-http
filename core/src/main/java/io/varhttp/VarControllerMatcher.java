a77ab629fff0bde4c0c135e5e97723540a6a25c9
andre-psqr <andre.vigneaux@psqr.eu>
2021-11-19 10:36:47 +0100

    make GET default http method as it doesnt make much sense to have a controller for all the http methods

--- core/src/main/java/io/varhttp/VarControllerMatcher.java
+++ core/src/main/java/io/varhttp/VarControllerMatcher.java
@@ -12,9 +12,6 @@ public class VarControllerMatcher implements ControllerMatcher {
 		Controller controllerAnnotation = method.getAnnotation(Controller.class);
 		if (controllerAnnotation != null) {
 			List<HttpMethod> httpMethods = Arrays.asList(controllerAnnotation.httpMethods());
-			if (httpMethods.isEmpty()) {
-				httpMethods = Arrays.asList(HttpMethod.values());
-			}
 			return Optional.of(new ControllerMatch(method, controllerAnnotation.path(), new HashSet<>(httpMethods), controllerAnnotation.contentType()));
 		}
 		return Optional.empty();
