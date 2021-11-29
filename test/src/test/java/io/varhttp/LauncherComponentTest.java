480a9471af9eccc074fc40ae7a93e5642874d23c
roman <roman.sarakvasa@persequor.com>
2021-11-29 09:47:39 +0100

    Instantiate servlet once, client on every test

--- test/src/test/java/io/varhttp/LauncherComponentTest.java
+++ test/src/test/java/io/varhttp/LauncherComponentTest.java
@@ -2,19 +2,27 @@ package io.varhttp;
 
 import io.odinjector.OdinJector;
 import io.varhttp.test.VarClientServerless;
+import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class LauncherComponentTest extends LauncherTestBase {
 
+
+	private static Serializer serializer;
+	private static VarServlet varServlet;
+
 	@BeforeClass
-	public static void setup() throws Throwable {
+	public static void classSetup() {
 		OdinJector odinJector = OdinJector.create().addContext(new OdinContext(new VarConfig().setPort(8088)));
 		Launcher launcher = odinJector.getInstance(Launcher.class);
-		VarServlet varServlet = launcher.getServlet();
+		varServlet = launcher.getServlet();
 		varServlet.configure(launcher::configure);
 
-		Serializer serializer = odinJector.getInstance(Serializer.class);
+		serializer = odinJector.getInstance(Serializer.class);
+	}
 
+	@Before
+	public void setup() throws Throwable {
 		varClient = new VarClientServerless(varServlet, serializer);
 	}
 }
