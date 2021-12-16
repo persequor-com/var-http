a8aac856bf756b330eb885600d2614c9618fe091
roman <roman.sarakvasa@persequor.com>
2021-12-16 13:54:39 +0100

    Decode url string after splitting it into the parameters

--- core/src/main/java/io/varhttp/HttpHelper.java
+++ core/src/main/java/io/varhttp/HttpHelper.java
@@ -13,14 +13,22 @@ import static java.util.stream.Collectors.*;
 
 public class HttpHelper {
 
-	public static Map<String, List<String>> parseQueryString(String queryString) throws UnsupportedEncodingException {
+	public static Map<String, List<String>> parseQueryString(String queryString) {
 		if (queryString == null) {
 			return Collections.emptyMap();
 		}
-		return Stream.of(URLDecoder.decode(queryString, Charsets.UTF_8.toString())
-						.split("&")).map(s -> s.split("="))
+		return Stream.of(queryString
+						.split("&")).map(s -> s.split("=", 3))
 				.filter(keyValue -> keyValue.length == 2)
-				.collect(groupingBy(keyValue -> keyValue[0].trim(), mapping(keyValue -> keyValue[1].trim(), toList())));
+				.collect(groupingBy(keyValue -> silentDecode(keyValue[0]).trim(), mapping(keyValue -> silentDecode(keyValue[1]).trim(), toList())));
+	}
+
+	private static String silentDecode(String input) {
+		try {
+			return URLDecoder.decode(input, Charsets.UTF_8.toString());
+		} catch (UnsupportedEncodingException e) {
+			throw new IllegalStateException("Unable to decode part of query string", e);
+		}
 	}
 
 }
---
+++ core/src/test/java/io/varhttp/HttpHelperTest.java
@@ -0,0 +1,38 @@
+package io.varhttp;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class HttpHelperTest {
+
+	@Test
+	public void parseQueryString_happy() throws Throwable{
+		Map<String, List<String>> actual = HttpHelper.parseQueryString("my-param=var-http%20is%20awesome&my-param=cool&my-second-param=hello");
+
+		assertEquals(Arrays.asList("var-http is awesome", "cool"), actual.get("my-param"));
+		assertEquals(Arrays.asList("hello"), actual.get("my-second-param"));
+	}
+
+	@Test
+	public void parseQueryString_encodedSeparators() throws Throwable{
+		Map<String, List<String>> actual = HttpHelper.parseQueryString("my-param=var-http%3Dawesome%26cool&my-param=var-http%3Dawesome&var-http=cool&var-http=awesome%26cool");
+
+		assertEquals(Arrays.asList("cool", "awesome&cool"), actual.get("var-http"));
+		assertEquals(Arrays.asList("var-http=awesome&cool", "var-http=awesome"), actual.get("my-param"));
+	}
+
+	@Test
+	public void parseQueryString_ignoreBrokenParameters() throws Throwable{
+		Map<String, List<String>> actual = HttpHelper.parseQueryString("my-param&var-http=yyy=zzz&&only=good");
+
+		assertEquals(1, actual.size());
+		assertEquals(Arrays.asList("good"), actual.get("only"));
+	}
+
+
+}
