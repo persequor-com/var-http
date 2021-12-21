60915b15011e3ea0f5edee5c5d8574e12f7e1fb7
andre-psqr <andre.vigneaux@psqr.eu>
2021-12-21 12:59:08 +0100

    dont decode when parsing queryStrings by default

--- core/src/main/java/io/varhttp/HttpHelper.java
+++ core/src/main/java/io/varhttp/HttpHelper.java
@@ -13,12 +13,21 @@ import static java.util.stream.Collectors.*;
 
 public class HttpHelper {
 
-	public static Map<String, List<String>> parseQueryString(String queryString) throws UnsupportedEncodingException {
+	public static Map<String, List<String>> parseQueryStringEncoded(String queryString) throws UnsupportedEncodingException {
 		if (queryString == null) {
 			return Collections.emptyMap();
 		}
 		return Stream.of(URLDecoder.decode(queryString, Charsets.UTF_8.toString())
 						.split("&")).map(s -> s.split("="))
+				.filter(keyValue -> keyValue.length == 2)
+				.collect(groupingBy(keyValue -> keyValue[0].trim(), mapping(keyValue -> keyValue[1].trim(), toList())));
+	}
+
+	public static Map<String, List<String>> parseQueryString(String queryString) throws UnsupportedEncodingException {
+		if (queryString == null) {
+			return Collections.emptyMap();
+		}
+		return Stream.of(queryString.split("&")).map(s -> s.split("="))
 				.filter(keyValue -> keyValue.length == 2)
 				.collect(groupingBy(keyValue -> keyValue[0].trim(), mapping(keyValue -> keyValue[1].trim(), toList())));
 	}
