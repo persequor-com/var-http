9a9b2ea7cd7f4aef799c94d34236cd11ab06ea14
roman <roman.sarakvasa@persequor.com>
2023-05-04 14:29:10 +0200

    OPEN-88 Synchronize list since it is used in different thread contexts

--- var-test/src/main/java/io/varhttp/controllers/withfilters/FilterCatcher.java
+++ var-test/src/main/java/io/varhttp/controllers/withfilters/FilterCatcher.java
@@ -3,17 +3,18 @@ package io.varhttp.controllers.withfilters;
 import javax.inject.Singleton;
 import javax.servlet.Filter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Singleton
 public class FilterCatcher {
-	List<String> result = new ArrayList<>();
+	private final List<String> result = Collections.synchronizedList(new ArrayList<>());
 
 	public void add(String result) {
 		this.result.add(result);
 	}
 
 	public List<String> getResult() {
-		return result;
+		return this.result;
 	}
 }
