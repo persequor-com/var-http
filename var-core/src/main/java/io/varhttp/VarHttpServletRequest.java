350541e641a56a9fb7ea57c979d55b9eb734c171
roman <roman.sarakvasa@persequor.com>
2023-05-17 10:48:47 +0200

    Streamline var implementation of HttpServletRequest wrapper with old standalone implementation.
    Block session related methods, since pluggable session storage is not yet included into VAR

--- var-core/src/main/java/io/varhttp/VarHttpServletRequest.java
+++ var-core/src/main/java/io/varhttp/VarHttpServletRequest.java
@@ -1,7 +1,12 @@
 package io.varhttp;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.Principal;
 
 public class VarHttpServletRequest extends HttpServletRequestWrapper {
 
@@ -23,5 +28,76 @@ public class VarHttpServletRequest extends HttpServletRequestWrapper {
 		}
 
 		return super.isSecure();
+	}
+
+	@Override
+	public String getRemoteUser() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public boolean isUserInRole(String role) {
+		return false;
+	}
+
+	@Override
+	public Principal getUserPrincipal() {
+		return () -> "Anonymous";
+	}
+
+	@Override
+	public String getRequestedSessionId() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+
+	@Override
+	public HttpSession getSession(boolean create) {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public HttpSession getSession() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public String changeSessionId() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public boolean isRequestedSessionIdValid() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public boolean isRequestedSessionIdFromCookie() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public boolean isRequestedSessionIdFromURL() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public boolean isRequestedSessionIdFromUrl() {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public void login(String username, String password) throws ServletException {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
+	}
+
+	@Override
+	public void logout() throws ServletException {
+		throw new UnsupportedOperationException("Not yet implemented in var-http");
 	}
 }
